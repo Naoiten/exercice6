1b9fcc75ac1226fccb830b308401dd5ab1361a43
Antoine <antoine.hotiin32@laposte.net>
2019-07-19 10:45:43 +0200

    exercice finis

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var number;
+function texte(){
+  number=document.getElementById('number').value;
+  window.alert(number*10);
+}
+
+function supprimer(){
+  document.getElementById('number').value=""
+}
